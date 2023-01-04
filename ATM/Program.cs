9f4f2439f0ee0a119df6ce9eaa6df8df31c675c9
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2023-01-04 20:38:24 +0000

    declare types of variables

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+public class CardHolder
+{
+    String cardNumber;
+    int pin;
+    String firstName;
+    String lastName;
+    double balance;
+
+    public CardHolder(String cardNumber, int pin, String firstName, String lastName, double balance)
+    {
+        this.cardNumber = cardNumber;
+        this.pin = pin;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.balance = balance;
+    }
+
+}
